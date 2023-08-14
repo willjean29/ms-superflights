abf11e9f95602325afc24efdc30693546aba9b2c
Jean Osco <willjean29@gmail.com>
2023-08-14 11:50:49 -0500

    feat(api-gateway): configure validate pipe global

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { AllExceptionFilter } from './common/filters/http-exception.filter';
 import { TimeOutInterceptor } from './common/interceptors/timeout.interceptors';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
@@ -11,6 +11,12 @@ async function bootstrap() {
   app.setGlobalPrefix('/api/v2');
   app.useGlobalFilters(new AllExceptionFilter())
   app.useGlobalInterceptors(new TimeOutInterceptor())
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
   const config = new DocumentBuilder()
     .setTitle('MS SuperFlight API Documentation')
     .setDescription('Scheduled Flight app')
